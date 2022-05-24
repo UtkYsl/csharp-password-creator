dd17124db652f4a15d9af96ce7f78ed20f65f3bc
UtkYsl <97233234+UtkYsl@users.noreply.github.com>
2022-05-24 22:50:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿char[] karakter = "abcdefghijklmnoprstuvwxyz0123456789+*/-@#&".ToArray();
+string result = string.Empty;
+Console.WriteLine("Oluşturulacak şifrenin uzunluğunu girin.");
+int karaktersayısı = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+for (int i= 0; i < karaktersayısı; i++)
+{
+    result += karakter[rand.Next(0, karakter.Length - 1)].ToString();
+}
+Console.WriteLine(result);
